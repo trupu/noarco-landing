a2f01e75bed6249131d5d968c7849ebd9326b3b2
trupu <trupeusz@gmail.com>
2024-01-31 22:33:40 +0100

    fix: styles

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,77 @@
-import './style.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+// import './style.css'
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-// import "bootstrap/sass/bootstrap.scss";
+
+import { gsap } from 'gsap';
+import SplitType from 'split-type';
+
+const textFromLeftObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && entry.target.gsapChars) {
+            // console.log(entry.target);            
+            gsap.fromTo(
+                entry.target.gsapChars,
+                { 
+                  y: 10,
+                  opacity: 0
+                },
+                {
+                  y: 0,
+                  opacity: 1,
+                  stagger: 0.03,
+                  duration: 1,
+                  ease: 'power4.out',
+                }
+              );
+        }
+    });
+});
+
+const imageObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animation-image--active');
+        } else {
+            entry.target.classList.remove('animation-image--active');
+        }
+    });
+});
+
+const textFromLeftElements = document.querySelectorAll('.animation-text-from-left');
+const imageElements = document.querySelectorAll('.animation-image');
+
+textFromLeftElements.forEach(el => {
+    const ourText = new SplitType(el, {
+        types: 'chars',
+    });
+    el.gsapChars = ourText.chars;
+});
+
+textFromLeftElements.forEach(element => {
+    textFromLeftObserver.observe(element);
+});
+
+imageElements.forEach(element => {
+    imageObserver.observe(element);
+});
+
+const scrollToListener = () => {
+    const scrollToElements = document.querySelectorAll('.js-scroll-to');
+
+    scrollToElements.forEach(element => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const target = document.querySelector(element.dataset.target || element.getAttribute('href'));
+            
+            const fromTop = target.offsetTop;
+
+            window.scrollTo({
+                top: fromTop,
+                behavior: 'smooth',
+            });
+        });
+    });
+}
+
+scrollToListener();
